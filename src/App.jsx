be08e2a020f1ca95dcd3f1235afdafb5481da262
johir haque dipok <johirhaquedipok2@gmail.com>
2022-10-31 01:33:54 +0600

    update some css

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,6 @@ function App() {
       <CTA />
       <Blog />
       <Footer />
-      {/* https://www.figma.com/file/lz9lLpFHMxHm2odnwM3R0z/gpt3?node-id=0%3A15 */}
-      {/* https://github.com/adrianhajdin/project_modern_ui_ux_gpt3/blob/main/src/containers/header/Header.jsx */}
-      {/* https://github.com/adrianhajdin/project_modern_ui_ux_gpt3/blob/main/src/containers/header/header.css */}
     </div>
   );
 }
